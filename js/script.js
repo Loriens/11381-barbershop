1d41c6adaeb8382e6865d28f85492fb396f8c4c3
Loriens <lerd.95@mail.ru>
2016-08-01 11:06:41 +0300

    Add form for login and autoprefixes for CSS

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var link = document.querySelector(".login");
+
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
+
+var login = popup.querySelector("[name=login]");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+  login.focus();
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+});
